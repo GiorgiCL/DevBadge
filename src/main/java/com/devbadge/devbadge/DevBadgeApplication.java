1a26b48c26271d7e7afb9fd3683868efbdc0e4ff
GiorgiCL <s054865@ad.viko.lt>
2025-12-04 18:57:35 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/devbadge/devbadge/DevBadgeApplication.java
@@ -0,0 +1,13 @@
+package com.devbadge.devbadge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBadgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevBadgeApplication.class, args);
+    }
+
+}
